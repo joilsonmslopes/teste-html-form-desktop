101105ec1a5098730a9d54655f972d7f1ac8c57f
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2020-12-09 01:43:16 -0300

    chore: add event blur in field cep

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// const cep = document.querySelector("#cep");
+
+// cep.addEventListener("blur", (e) => {
+//     document.querySelector(".endereco-completo")
+//     .classList
+//     .toggle("hide")
+// });
